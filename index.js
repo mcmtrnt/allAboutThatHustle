d2641e5d22055b016d4bf9f4568ca29419a5690c
Trent McMillan <mcmtrnt3@gmail.com>
2022-07-21 22:29:55 -0600

    send emails

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+emailjs.init('LlEjWseFWWSRQTKr-');
+
 function onMouseOut(event) {
     // If the mouse is near the top of the window, show the popup
     // Also, do NOT trigger when hovering or clicking on selects
@@ -19,21 +21,39 @@ function signup() {
     console.log(document.getElementById('email-input').value);
     if (isValidEmail(document.getElementById('email-input').value) && isNonEmpty(document.getElementById('email-input'))) {
       console.log('test');
-      Email.send({
-        Host: "smtp.gmail.com",
-        Username : "email.alpinexc1@gmail.com",
-        Password : "mnxitdqmjbzhcywb", //AlpineXC1$$ mnxitdqmjbzhcywb
-        To : 'mcmtrnt3@gmail.com',  
-        From : "email.alpinexc1@gmail.com",
-        Subject : "All About That Hustle Newsletter Signup",
-        Body : document.getElementById('email-input').value + "     From: " + document.getElementById('email-input').value ,
-      })
-      .then(function(message){
-          document.getElementById('email-input').value = "";
-          document.getElementById('signup-error').style.display = "none";
-          signupoff();
-          // window.location.href = "confirmation.html";
+      
+      var templateParams = {
+        message: document.getElementById('email-input').value
+      };
+
+      emailjs.send('service_gntorr9', 'template_2y4riql', templateParams)
+      .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        document.getElementById('email-input').value = "";
+        document.getElementById('signup-error').style.display = "none";
+        signupoff();
+      }, function(error) {
+        console.log('FAILED...', error);
+        document.getElementById('email-input').value = "";
+        document.getElementById('signup-error').style.display = "none";
+        signupoff();
       });
+
+      // Email.send({
+      //   Host: "smtp.gmail.com",
+      //   Username : "email.alpinexc1@gmail.com",
+      //   Password : "mnxitdqmjbzhcywb", //AlpineXC1$$ mnxitdqmjbzhcywb
+      //   To : 'mcmtrnt3@gmail.com',  
+      //   From : "email.alpinexc1@gmail.com",
+      //   Subject : "All About That Hustle Newsletter Signup",
+      //   Body : document.getElementById('email-input').value + "     From: " + document.getElementById('email-input').value ,
+      // })
+      // .then(function(message){
+      //     document.getElementById('email-input').value = "";
+      //     document.getElementById('signup-error').style.display = "none";
+      //     signupoff();
+      //     // window.location.href = "confirmation.html";
+      // });
   
     }
     else {
